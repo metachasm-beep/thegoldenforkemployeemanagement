40dd583b7f2668ee7215c512c7a3a119351eb436
metachasm-beep <metachasm@gmail.com>
2026-08-30 02:56:31 +0530

    fix: use standard prisma client to prevent adapter crashes in vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import ws from 'ws';
-
-neonConfig.webSocketConstructor = ws;
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-// @ts-ignore
-const adapter = new PrismaNeon(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ adapter });
+  globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
