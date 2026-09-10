c841dc1cba94de1e7ff05adca099a5e821e387d2
metachasm-beep <metachasm@gmail.com>
2026-09-10 13:59:05 +0530

    fix(pwa): automatically prepend mailto: to VAPID_SUBJECT if missing

--- src/lib/webpush.ts
+++ src/lib/webpush.ts
@@ -1,7 +1,12 @@
 import webpush from "web-push";
 
+let subject = process.env.VAPID_SUBJECT || "mailto:contact@thegoldenfork.de";
+if (!subject.startsWith("mailto:") && !subject.startsWith("http")) {
+  subject = "mailto:" + subject;
+}
+
 webpush.setVapidDetails(
-  process.env.VAPID_SUBJECT || "mailto:contact@thegoldenfork.de",
+  subject,
   process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
   process.env.VAPID_PRIVATE_KEY as string
 );
