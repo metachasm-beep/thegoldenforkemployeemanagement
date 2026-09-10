486e6d8cc33c729158df700fc4320de76f9455c8
metachasm-beep <metachasm@gmail.com>
2026-09-10 13:40:57 +0530

    chore(pwa): update web push vapid subject to use active contact email

--- src/lib/webpush.ts
+++ src/lib/webpush.ts
@@ -1,7 +1,7 @@
 import webpush from "web-push";
 
 webpush.setVapidDetails(
-  process.env.VAPID_SUBJECT || "mailto:admin@goldenfork.com",
+  process.env.VAPID_SUBJECT || "mailto:contact@thegoldenfork.de",
   process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
   process.env.VAPID_PRIVATE_KEY as string
 );
